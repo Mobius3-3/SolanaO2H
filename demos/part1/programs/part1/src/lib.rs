b0797513fdc2d709c52f8e93ca2b452f8e6edd54
AndrewFang <imfangcong@gmail.com>
2025-04-15 12:11:04 +0700

    Add: part1 demo code new storage logic

--- demos/part1/programs/part1/src/lib.rs
+++ demos/part1/programs/part1/src/lib.rs
@@ -1,21 +1,44 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
 declare_id!("2FtFkSFRV2GXQt4mhHHJxUMdZGHa7TqaKEkGUW9eYcoi");
 
 #[program]
-pub mod part1 {
+pub mod basic_storage {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Set>, new_value1: u64) -> Result<()> {
+        ctx.accounts.my_storage.value1 = new_value1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Set<'info> {
+    #[account(mut, seeds = [], bump)]
+    pub my_storage: Account<'info, MyStorage>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init,
+              payer = signer,
+              space=size_of::<MyStorage>() + 8,
+              seeds = [],
+              bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct MyStorage {
-    value1: u8,
+    value1: u64,
 }
